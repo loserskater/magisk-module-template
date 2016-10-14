05104795fad52652bb96736d7cd0d1640149a715
Stan Grishin <stangri@stangri-mba42.local>
2016-10-14 05:41:22 -0700

    1.1.0

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 This module converts a selected list of installed apps to system apps.
 
 # Supported Apps
-Action Launcher
-Pixel Launcher (and its Wallpaper Picker)
-Project Fi
+* Action Launcher
+* Pixel Launcher (and its Wallpaper Picker)
+* Project Fi
+* Cerberus Anti Theft
--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -1,7 +1,14 @@
 #!/system/bin/sh
 
-# This script will be executed in post-fs-data mode
-# More info in the main Magisk thread
+MODPATH=/magisk/AppSystemizer
+apps=(
+		"NexusLauncherPrebuilt com.google.android.apps.nexuslauncher priv-app" 
+		"WallpaperPickerGooglePrebuilt com.google.android.apps.wallpaper app"
+		"Tycho com.google.android.apps.tycho app"
+		"ActionLauncher com.actionlauncher.playstore priv-app" 
+		"CerberusAntiTheft com.lsdroid.cerberus priv-app" 
+)
+permreset=
 
 set_perm() {
   chown $2:$3 $1 || exit 1
@@ -22,42 +29,79 @@ set_perm_recursive() {
   done
 }
 
-SPRIV=/system/priv-app
-MPRIV=/magisk/AppSystemizer/system/priv-app
-SAPP=/system/app
-MAPP=/magisk/AppSystemizer/system/app
-permreset=
+travel() {
+  cd $1/$2
+  if [ -f ".replace" ]; then
+    rm -rf $TMPDIR/$2
+    mktouch $TMPDIR/$2 $1
+  else
+    for ITEM in * ; do
+      if [ ! -e "/$2/$ITEM" ]; then
+        # New item found
+        if [ $2 = "system" ]; then
+          # We cannot add new items to /system root, delete it
+          rm -rf $ITEM
+        else
+          if [ -d "$TMPDIR/dummy/$2" ]; then
+            # We are in a higher level, delete the lower levels
+            rm -rf $TMPDIR/dummy/$2
+          fi
+          # Mount the dummy parent
+          mktouch $TMPDIR/dummy/$2
 
-if [ ! -d ${SPRIV}/NexusLauncherPrebuilt -a ! -d ${MPRIV}/NexusLauncherPrebuilt -a -d /data/app/com.google.android.apps.nexuslauncher* ]; then 
-	mkdir -p ${MPRIV}/NexusLauncherPrebuilt
-	for i in /data/app/com.google.android.apps.nexuslauncher*/base.apk; do
-		cp $i ${MPRIV}/NexusLauncherPrebuilt/NexusLauncherPrebuilt.apk
-	done
-	permreset=1
+          mkdir -p $DUMMDIR/$2 2>/dev/null
+          if [ -d "$ITEM" ]; then
+            # Create new dummy directory
+            mkdir -p $DUMMDIR/$2/$ITEM
+          elif [ -L "$ITEM" ]; then
+            # Symlinks are small, copy them
+            cp -afc $ITEM $DUMMDIR/$2/$ITEM
+          else
+            # Create new dummy file
+            mktouch $DUMMDIR/$2/$ITEM
+          fi
+
+          # Clone the original /system structure (depth 1)
+          if [ -e "/$2" ]; then
+            for DUMMY in /$2/* ; do
+              if [ -d "$DUMMY" ]; then
+                # Create dummy directory
+                mkdir -p $DUMMDIR$DUMMY
+              elif [ -L "$DUMMY" ]; then
+                # Symlinks are small, copy them
+                cp -afc $DUMMY $DUMMDIR$DUMMY
+              else
+                # Create dummy file
+                mktouch $DUMMDIR$DUMMY
+              fi
+            done
+          fi
+        fi
+      fi
+
+      if [ -d "$ITEM" ]; then
+        # It's an directory, travel deeper
+        (travel $1 $2/$ITEM)
+      elif [ ! -L "$ITEM" ]; then
+        # Mount this file
+        mktouch $TMPDIR/$2/$ITEM $1
+      fi
+    done
+  fi
+}
+
+for line in "${apps[@]}"; do 
+	IFS=' ' read name canonical path <<< $line
+	if [ ! -d /system/${path}/${name} -a ! -d ${MODPATH}/system/${path}/${name} -a -d /data/app/${canonical}* ]; then 
+		mkdir -p ${MODPATH}/system/${path}/${name}
+		for i in /data/app/${canonical}*/base.apk; do
+			cp $i ${MODPATH}/system/${path}/${name}/${name}.apk
+		done
+		permreset=1
+	fi
+done
+
+if [ ! -z "$permreset"]; then
+	set_perm_recursive ${MODPATH}/system 0 0 0755 0644
+	travel $MODPATH system
 fi
-
-if [ ! -d ${SAPP}/WallpaperPickerGooglePrebuilt -a ! -d ${MAPP}/WallpaperPickerGooglePrebuilt -a -d /data/app/com.google.android.apps.wallpaper* ]; then
-	mkdir -p ${MAPP}/WallpaperPickerGooglePrebuilt
-	for i in /data/app/com.google.android.apps.wallpaper*/base.apk; do
-		cp $i ${MAPP}/WallpaperPickerGooglePrebuilt/WallpaperPickerGooglePrebuilt.apk
-	done
-	permreset=1
-fi
-
-if [ ! -d ${SAPP}/Tycho -a ! -d ${MAPP}/Tycho -a -d /data/app/com.google.android.apps.tycho* ]; then
-	mkdir -p ${MAPP}/Tycho
-	for i in /data/app/com.google.android.apps.tycho*/base.apk; do
-		cp $i ${MAPP}/Tycho/Tycho.apk
-	done
-	permreset=1
-fi
-
-if [ ! -d ${SPRIV}/ActionLauncher -a ! -d ${MPRIV}/ActionLauncher -a -d /data/app/com.actionlauncher.playstore* ]; then
-	mkdir -p ${MPRIV}/ActionLauncher
-	for i in /data/app/com.actionlauncher.playstore*/base.apk; do
-		cp $i ${MPRIV}/ActionLauncher/ActionLauncher.apk
-	done
-	permreset=1
-fi
-
-[ ! -z "$permreset" ] && set_perm_recursive /magisk/AppSystemizer/system 0 0 0755 0644
