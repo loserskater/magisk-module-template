5126c5c1cf192b1723449f3a58a5f48aa88ff582
Stan Grishin <stangri@melmac.net>
2017-01-02 14:18:49 -0800

    v10-compatible template.

--- config.sh
+++ config.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 MODID=AppSystemizer
 AUTOMOUNT=true
 POSTFSDATA=true
@@ -11,4 +12,106 @@ print_modname() {
 
 set_permissions() {
   set_perm_recursive  $MODPATH  0  0  0755  0644
+=======
+##########################################################################################
+#
+# Magisk
+# by topjohnwu
+# 
+# This is a template zip for developers
+#
+##########################################################################################
+##########################################################################################
+# 
+# Instructions:
+# 
+# 1. Place your files into system folder (delete the placeholder file)
+# 2. Fill in your module's info into module.prop
+# 3. Configure the settings in this file (common/config.sh)
+# 4. For advanced features, add shell commands into the script files under common:
+#    post-fs-data.sh, service.sh
+# 5. For changing props, add your additional/modified props into common/system.prop
+# 
+##########################################################################################
+
+##########################################################################################
+# Defines
+##########################################################################################
+
+# NOTE: This part has to be adjusted to fit your own needs
+
+# This will be the folder name under /magisk
+# This should also be the same as the id in your module.prop to prevent confusion
+MODID=template
+
+# Set to true if you need to enable Magic Mount
+# Most mods would like it to be enabled
+AUTOMOUNT=true
+
+# Set to true if you need to load system.prop
+PROPFILE=false
+
+# Set to true if you need post-fs-data script
+POSTFSDATA=false
+
+# Set to true if you need late_start service script
+LATESTARTSERVICE=false
+
+##########################################################################################
+# Installation Message
+##########################################################################################
+
+# Set what you want to show when installing your mod
+
+print_modname() {
+  ui_print "*******************************"
+  ui_print "     Magisk Module Template    "
+  ui_print "*******************************"
+}
+
+##########################################################################################
+# Replace list
+##########################################################################################
+
+# List all directories you want to directly replace in the system
+# By default Magisk will merge your files with the original system
+# Directories listed here however, will be directly mounted to the correspond directory in the system
+
+# This is an example (these values shall be overwritten)
+REPLACE="
+/system/app/Youtube
+/system/priv-app/SystemUI
+/system/priv-app/Settings
+/system/framework
+"
+
+# Construct your own list here
+# !!! Don't remove this if you don't need to replace anything, leave it empty
+# It shall overwrite the example
+REPLACE="
+"
+
+##########################################################################################
+# Permissions
+##########################################################################################
+
+# NOTE: This part has to be adjusted to fit your own needs
+
+set_permissions() {
+  # Default permissions, don't remove them
+  set_perm_recursive  $MODPATH  0  0  0755  0644
+
+  # Only some special files require specific permissions
+  # The default permissions should be good enough for most cases
+
+  # Some templates if you have no idea what to do:
+
+  # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
+  # set_perm_recursive  $MODPATH/system/lib       0       0       0755            0644
+
+  # set_perm  <filename>                         <owner> <group> <permission> <contexts> (default: u:object_r:system_file:s0)
+  # set_perm  $MODPATH/system/bin/app_process32   0       2000    0755         u:object_r:zygote_exec:s0
+  # set_perm  $MODPATH/system/bin/dex2oat         0       2000    0755         u:object_r:dex2oat_exec:s0
+  # set_perm  $MODPATH/system/lib/libart.so       0       0       0644
+>>>>>>> upstream/master
 }
